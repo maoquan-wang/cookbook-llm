628d7c8ef1057a881cd544dc324afb7e045137c9
maoquan-wang <mqvvang@gmail.com>
2025-03-15 09:55:20 +0000

    mcp demo case

---
+++ src/mcp/demo/client.py
@@ -0,0 +1,64 @@
+import asyncio
+from contextlib import AsyncExitStack
+from typing import Optional
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+
+class MCPClient:
+    def __init__(self, server_script_path: str):
+        # Initialize session and client objects
+        self.session: Optional[ClientSession] = None
+        self.exit_stack = AsyncExitStack()
+        self.server_script_path = server_script_path
+
+    async def connect_to_server(self):
+        server_params = StdioServerParameters(command="python", args=[self.server_script_path], env=None)
+        stdio, write = await self.exit_stack.enter_async_context(stdio_client(server_params))
+        self.session = await self.exit_stack.enter_async_context(ClientSession(stdio, write))
+        await self.session.initialize()
+
+        # List available tools
+        response = await self.session.list_tools()
+        tools = response.tools
+        print("\nConnected to server with tools:", [tool.name for tool in tools])
+
+    async def test_tool(self, currency: str):
+        result = await self.session.call_tool("get_bitcoin_price", {"currency": currency})
+        print("Tool call result: \n")
+        print(result.content[0].text)
+        return result
+
+    async def chat_loop(self):
+        """Run an interactive chat loop"""
+        print("\nMCP Client Started!")
+        print("Type your queries or 'quit' to exit.")
+
+        while True:
+            try:
+                query = input("\nQuery: ").strip()
+                if query.lower() == "quit":
+                    break
+                else:
+                    await self.test_tool(query)
+            except Exception as e:
+                print(f"\nError: {str(e)}")
+
+    async def cleanup(self):
+        """Clean up resources"""
+        await self.exit_stack.aclose()
+
+
+async def main():
+    client = MCPClient("./my_server.py")
+    try:
+        await client.connect_to_server()
+        await client.chat_loop()
+    finally:
+        await client.cleanup()
+
+
+if __name__ == "__main__":
+    # python client.py
+    asyncio.run(main())
---
+++ src/mcp/demo/my_server.py
@@ -0,0 +1,70 @@
+import json
+from typing import Sequence
+
+from mcp.server import Server
+from mcp.server.stdio import stdio_server
+from mcp.shared.exceptions import McpError
+from mcp.types import EmbeddedResource, ImageContent, TextContent, Tool
+
+
+def get_bitcoin_price(currency: str) -> dict:
+    if currency == "USD":
+        return {"price": 83829, "currency": "USD"}
+    elif currency == "CNY":
+        return {"price": 83829 * 7, "currency": "CNY"}
+
+    return {"error": "Unsupported currency"}
+
+
+async def bitcoin_price_mcp_server() -> None:
+    server = Server("mcp-bitcoin-price")
+
+    @server.list_tools()
+    async def list_tools() -> list[Tool]:
+        """List available time tools."""
+        return [
+            Tool(
+                name="get_bitcoin_price",
+                description="Get current price of Bitcoin",
+                inputSchema={
+                    "type": "object",
+                    "properties": {
+                        "currency": {
+                            "type": "string",
+                            "description": "currency (e.g., 'USD', 'CNY').",
+                        }
+                    },
+                    "required": ["currency"],
+                },
+            ),
+        ]
+
+    @server.call_tool()
+    async def call_tool(name: str, arguments: dict) -> Sequence[TextContent | ImageContent | EmbeddedResource]:
+        """Handle tool calls for queries."""
+        try:
+            match name:
+                case "get_bitcoin_price":
+                    currency = arguments.get("currency", None)
+                    if not currency:
+                        raise ValueError("Missing required argument: currency")
+                    result = get_bitcoin_price(currency)
+                case _:
+                    raise ValueError(f"Unknown tool: {name}")
+            return [TextContent(type="text", text=json.dumps(result, indent=2))]
+        except Exception as e:
+            raise ValueError(f"Error processing query: {str(e)}")
+
+    options = server.create_initialization_options()
+    async with stdio_server() as (read_stream, write_stream):
+        await server.run(read_stream, write_stream, options)
+
+
+def main():
+    import asyncio
+
+    asyncio.run(bitcoin_price_mcp_server())
+
+
+if __name__ == "__main__":
+    main()
