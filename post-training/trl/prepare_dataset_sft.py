5aa925f50fb27f74bd3689b5c80916dc9a31e8e5
maoquan-wang <mqvvang@gmail.com>
2025-04-01 03:34:02 +0000

    add dataset token count describe

--- post-training/trl/prepare_dataset_sft.py
+++ post-training/trl/prepare_dataset_sft.py
@@ -37,5 +37,42 @@ def kodcode_v1_sft_r1(save_file: str):
     print(f"saved {n} example to {save_file}")
 
 
+def sft_token_distribution(data_dir: str, tokenizer_path: str):
+
+    import pandas as pd
+    from transformers import AutoTokenizer
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        tokenizer_path,
+        trust_remote_code=True,
+        use_fast=True,
+    )
+
+    def count_messages_tokens(example: dict):
+        ids = tokenizer.apply_chat_template(example["messages"], tokenize=True)
+        return len(ids)
+
+    ds = load_dataset(data_dir, split="train")
+    ds_count = ds.map(
+        lambda row: {
+            "token_count": count_messages_tokens(row),
+        },
+        num_proc=8,
+        remove_columns=ds.column_names,
+    )
+    # the distribution of token_count
+    df = pd.DataFrame(ds_count["token_count"])
+    des = df.describe(percentiles=[0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99])
+    print(des)
+    return des
+
+
 if __name__ == "__main__":
-    kodcode_v1_sft_r1("/tmp/sft_train.jsonl")
+    # set hf cache dir, the default is ~/.cache/huggingface
+    # export HF_HOME=/tmp/hf
+    kodcode_v1_sft_r1("/tmp/hf/dataset/tmp/sft_train.jsonl")
+
+    sft_token_distribution(
+        "/tmp/hf/dataset/tmp",
+        "/tmp/hf/Qwen2.5-Coder-0.5B-Instruct",
+    )
